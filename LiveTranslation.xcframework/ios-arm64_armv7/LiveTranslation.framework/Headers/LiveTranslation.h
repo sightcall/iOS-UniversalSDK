9f8127c98d816b1ef2db963c24c0284a170e9847
Kireyin <kireyin@icloud.com>
2021-07-29 16:25:06 +0200

    New Frameworks

---
+++ LiveTranslation.xcframework/ios-arm64_armv7/LiveTranslation.framework/Headers/LiveTranslation.h
@@ -0,0 +1,18 @@
+//
+//  LiveTranslation.h
+//  LiveTranslation
+//
+//  Created by dpetrovic on 20/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveTranslation.
+FOUNDATION_EXPORT double LiveTranslationVersionNumber;
+
+//! Project version string for LiveTranslation.
+FOUNDATION_EXPORT const unsigned char LiveTranslationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveTranslation/PublicHeader.h>
+
+
---
+++ Multiparty.xcframework/ios-arm64_armv7/Multiparty.framework/Headers/Multiparty.h
@@ -0,0 +1,18 @@
+//
+//  Multiparty.h
+//  Multiparty
+//
+//  Created by Alexandre on 05/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Multiparty.
+FOUNDATION_EXPORT double MultipartyVersionNumber;
+
+//! Project version string for Multiparty.
+FOUNDATION_EXPORT const unsigned char MultipartyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Multiparty/PublicHeader.h>
+
+
