f63cc860b42c9bc7eee41550c8e7fcc7badb3c6f
Kireyin <kireyin@icloud.com>
2021-08-05 16:15:56 +0200

    2.6.2

---
+++ LiveTranslation.xcframework/ios-arm64_armv7/LiveTranslation.framework/Headers/LiveTranslation-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "LiveTranslation.h"
+
+FOUNDATION_EXPORT double LiveTranslationVersionNumber;
+FOUNDATION_EXPORT const unsigned char LiveTranslationVersionString[];
+
